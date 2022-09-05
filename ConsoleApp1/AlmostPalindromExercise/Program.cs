36d8cbac8e3a75f34c127091b1cf8254efe5ebf7
TodorPelivanov <tpelivanov@gmail.com>
2022-09-05 21:58:14 +0200

    exercises_pushed

---
+++ ConsoleApp1/AlmostPalindromExercise/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+
+namespace AlmostPalindromExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = MainLogic("htyyrghhuryyth"); //true
+            var result2 = MainLogic("kayak"); //false
+            var result3 = MainLogic("todor"); //true
+            var result4 = MainLogic("exercise"); //fasle
+            Console.WriteLine(result);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
+            Console.WriteLine(result4);
+        }
+
+        private static bool MainLogic(string input) 
+        {
+            if (IsPalindrome(input)) 
+            {
+                return false;
+            }
+            
+            var inputSplited = input.ToCharArray();
+
+            return LetterChanger(inputSplited);
+
+        }
+
+        private static bool LetterChanger(char[] input)
+        {
+            char[] az = Enumerable.Range('a', 'z' - 'a' + 1).Select(i => (Char)i).ToArray();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var tempstring = new String(input).Remove(i, 1);
+
+                for (int x = 0; x < az.Length; x++)
+                {             
+                    var insertTemp = tempstring.Insert(i, az[x].ToString());
+
+                    if (IsPalindrome(insertTemp)) 
+                    {
+                        return true;
+                    }
+                }
+
+            }
+
+            return false;
+        }
+
+        private static bool IsPalindrome(string input) 
+        {
+            if (input == new string(input.Reverse().ToArray())) 
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ ConsoleApp1/PasswordCheckerExercise/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PasswordCheckerExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> rawInput = new List<string>()
+            {
+                "a 1-3: abcde",
+                "b 1-3: cdefg",
+                "c 2-9: ccccccccc"
+            };
+
+            var invalidPasswords = 0;
+
+            foreach (var item in rawInput)
+            {
+                if (!CheckPassword(item))
+                {
+                    invalidPasswords++;
+                }
+            }
+
+            Console.WriteLine(invalidPasswords);
+        }
+
+
+        public static bool CheckPassword(string input)
+        {
+            var inputSplited = input.Split(null);
+            var letter = inputSplited[0].ToCharArray().First();
+            var criteria = inputSplited[1];
+            var password = inputSplited[2];
+
+            if (password.Contains(letter))
+            {
+                var numberOfLetters = password.Count(x => x == letter);
+                var splitedCriteria = criteria.Split(":").First().Split("-");
+
+                if (numberOfLetters >= int.Parse(splitedCriteria.First()) || numberOfLetters <= int.Parse(splitedCriteria.Last()))
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
